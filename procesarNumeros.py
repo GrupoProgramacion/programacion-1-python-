3e654cb81932f3891da5f8fd8fceb52f9fd77c3e
diegor8 <diegorosario2014@gmail.com>
2018-09-28 12:56:11 -0500

    Update procesarNumeros.py

--- procesarNumeros.py
+++ procesarNumeros.py
@@ -1,3 +1,37 @@
+“””” 
+Realice un programa, que permita al usuario ingresar varios números () enteros hasta que introduzca el numerocero.
+
+Luego mostrar lo siguiente:
+
+La cantidad de números leídos, sin incluir al cero. La cantidad de números pares La cantidad de números impares
+
+Input Format
+
+Cada línea representa un número
+
+Constraints
+
+Output Format
+
+Si las entradas no son correctas, mostrar "Invalid input". En caso contrario: La cantidad de números leídos, sin incluir al cero. La cantidad de números pares La cantidad de números impares
+
+Sample Input 0
+
+1
+1
+1
+0
+
+Sample Output 0
+
+3
+0
+3
+
+
+…
+“”” 
+
 from functools import reduce
 
 def validInput():
